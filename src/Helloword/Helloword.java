4a01e6bacfef65d8ae5a83054df033cc891fb971
dqqg <1248321610@qq.com>
2017-06-30 15:35:06 +0800

    sssssss

---
+++ src/Helloword/Helloword.java
@@ -0,0 +1,10 @@
+package Helloword;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/Helloword/Helloword2.java
@@ -0,0 +1,11 @@
+package Helloword;
+
+public class Helloword2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaaaa");
+
+	}
+
+}
